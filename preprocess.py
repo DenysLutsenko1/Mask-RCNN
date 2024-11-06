5acd9870504d079ecef4aa0a3ab957270cc67d56
DenysLutsenko1 <denja18022006@gmail.com>
2024-11-07 00:29:25 +0200

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -3,9 +3,9 @@ import shutil
 
 class Preprocess:
     def rename_files(self, folder_path):
-        jpg_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.jpg')])
-        xml_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.xml')])
-
+        # Сортируем файлы для переименования
+        jpg_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.jpg') and os.path.isfile(os.path.join(folder_path, f))])
+        xml_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.xml') and os.path.isfile(os.path.join(folder_path, f))])
 
         for i, (jpg, xml) in enumerate(zip(jpg_files, xml_files), start=1):
             new_name = f"{i:05d}"
@@ -17,23 +17,28 @@ class Preprocess:
             
             os.rename(old_jpg_path, new_jpg_path)
             os.rename(old_xml_path, new_xml_path)
-    
-    def move_files(self, folder_path):
 
-        jpg_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.jpg')])
-        xml_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.xml')])
+    def move_files(self, folder_path):
+        # Создаем папки, если они не существуют
+        os.makedirs(os.path.join(folder_path, "images"), exist_ok=True)
+        os.makedirs(os.path.join(folder_path, "annots"), exist_ok=True)
+
+        # Фильтруем файлы, чтобы перемещать только из корневого каталога, не затрагивая подпапки
+        jpg_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.jpg') and os.path.isfile(os.path.join(folder_path, f))])
+        xml_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.xml') and os.path.isfile(os.path.join(folder_path, f))])
 
         for file in jpg_files:
-            shutil.move(os.path.join(folder_path, file) , os.path.join(folder_path, "images", file))
-        
+            shutil.move(os.path.join(folder_path, file), os.path.join(folder_path, "images", file))
+
         for file in xml_files:
-            shutil.move(os.path.join(folder_path, file) , os.path.join(folder_path, "annots", file))
+            shutil.move(os.path.join(folder_path, file), os.path.join(folder_path, "annots", file))
 
 
+# Пример использования
 preprocess = Preprocess()
 
-preprocess.move_files("cobble")
+# Сначала переименуем файлы
+preprocess.rename_files("train")
 
-
-
-
+# Затем переместим файлы в папки images и annots
+preprocess.move_files("train")
